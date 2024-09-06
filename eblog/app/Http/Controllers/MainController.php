c95435d0dbe2d8328efee55d6f49b6cc0d0f5cf8
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-09-06 17:11:19 +0300

    some mods

--- eblog/app/Http/Controllers/MainController.php
+++ eblog/app/Http/Controllers/MainController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index(){
+        return ('this is index from main controller');
+    }
+
+    
+    public function about(){
+        return ('this is about from main controller ');
+    }
 }
--- eblog/routes/web.php
+++ eblog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('about', function () {
-    // return view('about');
-});
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/about', [MainController::class, 'about']);
 
 Route:: get('news/{id}', function () {
     return 'news';
